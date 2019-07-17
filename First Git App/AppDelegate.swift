e7b6aae03356faa4018e2b832e347107f25486d4
Ashot Galstian <ashot.galstian@secretsales.com>
2019-07-17 18:47:34 +0300

    Initial Commit

---
+++ First Git App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First Git App
+//
+//  Created by Ashot Galstyan on 7/17/19.
+//  Copyright © 2019 Ashot Galstyan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First Git App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First Git App
+//
+//  Created by Ashot Galstyan on 7/17/19.
+//  Copyright © 2019 Ashot Galstyan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
